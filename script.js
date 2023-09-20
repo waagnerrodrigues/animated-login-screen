06c09f4b4fe85681919a5ce2f951e92e0a1794f6
Wágner Rodrigues <144973105+waagnerrodrigues@users.noreply.github.com>
2023-09-20 00:47:52 -0300

    We have added images to enhance the project.

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function trocarImagemNome() {
-    var imagem = document.getElementById("imagem");
-    var campoNome = document.querySelector('input[type="text"]');
+    let imagem = document.getElementById("imagem");
+    let campoNome = document.querySelector('input[type="text"]');
     if (campoNome.value === "") {
-                imagem.src = "ursoolhoaberto.png";
+                imagem.src = "imagens/ursoolhoaberto.png";
     } else {
-                imagem.src = "ursoolhoaberto.png"; 
+                imagem.src = "imagens/ursoolhoaberto.png"; 
     }
  }
 
 function trocarImagemSenha() {
-    var imagem = document.getElementById("imagem");
-    var campoSenha = document.querySelector('input[type="password"]');
+    let imagem = document.getElementById("imagem");
+    let campoSenha = document.querySelector('input[type="password"]');
     if (campoSenha.type === "password") {
         campoSenha.type = "password"; 
-        imagem.src = "ursoolhofechado.png"; 
+        imagem.src = "imagens/ursoolhofechado.png"; 
     } else {
         campoSenha.type = "password"; 
-        imagem.src = "ursoolhoaberto.png"; 
+        imagem.src = "imagens/ursoolhoaberto.png"; 
     }
 }
 
 function restaurarImagem() {
-    var imagem = document.getElementById("imagem");
-    var campoSenha = document.querySelector('input[type="password"]');
+    let imagem = document.getElementById("imagem");
+    let campoSenha = document.querySelector('input[type="password"]');
     if (campoSenha.type === "text") {
         campoSenha.type = "password"; 
-        imagem.src = "ursoolhoaberto.png"; 
+        imagem.src = "imagens/ursoolhoaberto.png"; 
     }
 }
